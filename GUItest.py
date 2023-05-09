308cbefa2efc663387810a1009297b135c91cfae
DAMPFERS <razuvaevvladislav060@gmail.com>
2023-05-09 20:06:51 +0700

    None

--- GUItest.py
+++ GUItest.py
@@ -1,1 +1,21 @@
-print("0_o")
+from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
+from un import Ui_MainWindow
+
+# Create aplication
+app = QtWidgets.QApplication(sys.argv)
+
+# Create from and init UI
+MainWindow = QtWidgets.QMainWindow()
+ui = Ui_MainWindow()
+ui.setupUi(MainWindow)
+MainWindow.show()
+
+# Hook Logic
+def buttonPress():
+    ui.lineEdit.setText("\"Fuck\" YOU!")
+
+ui.B2.clicked.connect(buttonPress)
+
+# Run main loop 
+sys.exit(app.exec_())
